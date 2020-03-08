c6a58bf9b37ca6e253ec88cabe09b62c34ac1f5d
khchoi0301 <khchoi0301@gmail.com>
2020-03-09 00:06:46 +0900

    Using the getRequestHandler, Adding nodemon

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const express = require("express");
 const dev = process.env.NODE_ENV !== "production"
 const port = process.env.PORT || 5000;
 const app = next({ dev })
+const handle = app.getRequestHandler();
+
 
 app.prepare().then(() => {
     const server = express()
+
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    });
 
     server.listen(port, err => {
         if (err) throw err;
