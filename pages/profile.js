c4e803b1f63978537e2b9c63974f895bbc491e8c
khchoi0301 <khchoi0301@gmail.com>
2020-03-26 20:11:03 +0900

    Logging out Users, Clearing Cookies

--- pages/profile.js
+++ pages/profile.js
@@ -1,4 +1,4 @@
-import { getUserProfile } from "../lib/auth";
+import { getUserProfile, authInitialProps } from "../lib/auth";
 import Layout from '../components/Layout'
 
 export default class Profile extends React.Component {
@@ -15,10 +15,13 @@ export default class Profile extends React.Component {
     }
 
     render() {
+
         return (
-            <Layout title="Profile">
+            <Layout title="Profile" {...this.props} >
                 <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
             </Layout>
         )
     }
 }
+
+Profile.getInitialProps = authInitialProps();
